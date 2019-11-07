a6e8202a32b6008fd1539eb76f57cf6577abc89c
Eric Honkanen <eric.c.honkanen@gmail.com>
2019-11-06 21:44:07 -0800

    landshare initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    name='landshare',
+    package_dir={'': 'src'},
+    packages=find_packages(),
+    version='0.0.1',
+    zip_safe=False,
+)
